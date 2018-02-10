5c163a653a287a9942c8148a2008a0c7722f02a2
JETBLUE\HMaheshwari <himanshu.maheshwari@jetblue.com>
2018-02-10 17:41:13 +0530

    google pagenation at bootom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { DynamaicFormComponent } from './dynamaic-form/dynamaic-form.component';
 import { DynamaicTableComponent } from './dynamaic-table/dynamaic-table.component';
 import { JsonTableComponent } from './json-table/json-table.component';
 import {GetDataFromUrlService} from './get-data-from-url.service';
-import { PopoutModelComponent } from './popout-model/popout-model.component'
+import { PopoutModelComponent } from './popout-model/popout-model.component';
+import { PagenationComponent } from './pagenation/pagenation.component';
+import {PagerServiceService} from './pagenation/pager-service.service';
 @NgModule({
   declarations: [
     AppComponent,
     DynamaicFormComponent,
     DynamaicTableComponent,
     JsonTableComponent,
-    PopoutModelComponent
+    PopoutModelComponent,
+    PagenationComponent
   ],
   imports: [
     BrowserModule,FormsModule, ReactiveFormsModule,HttpModule
   ],
-  providers: [GetDataFromUrlService],
+  providers: [GetDataFromUrlService,PagerServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
