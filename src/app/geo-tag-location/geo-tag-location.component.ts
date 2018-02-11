b5f027cc574e67350bcd18248e6fb5ff6f5cc918
JETBLUE\HMaheshwari <himanshu.maheshwari@jetblue.com>
2018-02-11 10:53:43 +0530

    map with click event

--- src/app/geo-tag-location/geo-tag-location.component.ts
+++ src/app/geo-tag-location/geo-tag-location.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from '@agm/core/services/google-maps-types';
+import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-geo-tag-location',
@@ -7,17 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeoTagLocationComponent implements OnInit {
   title = 'My first AGM project';
-  lat = 23;
-  long = 75.809007;
+  draggable= true;
+  lat = 23.12;
+  lng = 75.54;
+  markers: marker[] = [
+    {
+		  lat: 51.673858,
+		  lng: 7.815982,
+		  label: 'A',
+		  draggable: true
+	  }
+  ];
+  mark: any= { };
   constructor() { }
 
   ngOnInit() {
   }
 
-  saveLocation(lat: number, long: number){
+  saveLocation(lat: number, lng: number){
     console.log(lat);
     this.lat = lat;
-    this.long = long;
-
+    this.lng = lng;
   }
+
+    mapClicked($event: MouseEvent){
+   
+    this.markers.push({
+        lat: $event.coords.lat,
+        lng: $event.coords.lng,
+        draggable: true});
+      console.log(this.markers);
+    }
+}
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
 }
